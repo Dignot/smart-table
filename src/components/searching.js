027c449c598f14e5162a3d7a5e07fe0361f68dd2
Wadim <pokinet@yandex.ru>
2025-08-04 22:21:00 +0300

    Completed

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,16 @@
 import {rules, createComparison} from "../lib/compare.js";
 
-
-export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-
+export function initSearching(elements, searchField) {
+    console.log(1)
     return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
+        const value = state[searchField];
+        if (!value) return data;
+        // Первый аргумент — массив строк-имен правил
+        // Второй аргумент — массив функций-правил
+        const compare = createComparison(
+            ['skipEmptyTargetValues'],
+            [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
+        );
+        return data.filter(row => compare(row, state));
     }
 }
